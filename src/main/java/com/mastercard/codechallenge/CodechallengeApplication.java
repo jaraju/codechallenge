7b3536cbec283418820c5269b046ddfe990cdc11
student <student@students-MacBook-Pro.local>
2020-08-31 14:44:05 -0400

    Enabling Swagger

--- src/main/java/com/mastercard/codechallenge/CodechallengeApplication.java
+++ src/main/java/com/mastercard/codechallenge/CodechallengeApplication.java
@@ -3,7 +3,10 @@ package com.mastercard.codechallenge;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class CodechallengeApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/mastercard/codechallenge/controller/ConnectCityController.java
+++ src/main/java/com/mastercard/codechallenge/controller/ConnectCityController.java
@@ -3,12 +3,14 @@ package com.mastercard.codechallenge.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mastercard.codechallenge.service.ConnectCityService;
 
 @RestController
+@RequestMapping("api")
 public class ConnectCityController {
 
 	@Autowired
